d01f76cce73776c35490868f583524dda5d293d0
Jennifer Weber <jkweber236@gmail.com>
2024-02-20 23:40:56 -0700

    Added menu items

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,85 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    var running = true
+    val recipe1 = Recipe().apply {
+        category = "Italian"
+        name = "Pasta Carbonara"
+        ingredients = "Spaghetti, Bacon, Eggs, Parmesan"
+        instructions = "Cook pasta, fry bacon, mix with eggs and Parmesan"
+    }
+
+
+    val recipe2 = Recipe().apply {
+        category = "Asian"
+        name = "Chicken Stir Fry"
+        ingredients = "Chicken, Vegetables, Soy Sauce, Rice"
+        instructions = "Stir-fry chicken and vegetables, add soy sauce, serve over rice"
+    }
+
+    val recipes = listOf(recipe1, recipe2)
+
+while (running) {
+        println("Main menu: ")
+        println("1. Add recipe")
+        println("2. Search for recipe")
+        println("3. Delete recipe")
+        println("4. Display all recipes")
+        println("5. Quit")
+        print("> ")
+        val userInput: String? = readLine()
+        val userChoice: Int = userInput?.toIntOrNull() ?: 0
+
+        when (userChoice) {
+            1 -> {
+                println("You selected 1")
+            }
+
+            2 -> {
+                println("Search by: ")
+                println("1. Name")
+                println("2. Category")
+                println("3. Ingredient")
+
+                val searchInput: String? = readLine()
+                val searchChoice: Int = searchInput?.toIntOrNull() ?: 0
+
+                when (searchChoice) {
+                    1 -> {
+                        println("You selected 1")
+                    }
+                    2 -> {
+                        println("You selected 2")
+                    }
+                    3 -> {
+                        println("You selected 3")
+                    }
+                }
+                println("You selected 2")
+            }
+
+            3 -> {
+                println("You selected 3")
+            }
+
+            4 -> {
+                for (recipe in recipes) {
+                    println("Category: ${recipe.category}")
+                    println("Name: ${recipe.name}")
+                    println("Ingredients: ${recipe.ingredients}")
+                    println("Instructions: ${recipe.instructions}\n")
+                }
+            }
+
+            5 -> {
+                println("Quitting...")
+                running = false
+            }
+
+            else -> {
+                println("Invalid choice. Please enter a number from 1 to 5.")
+            }
+        }
     }
 }
---
+++ src/Recipe.kt
@@ -0,0 +1,7 @@
+class Recipe {
+    var category = ""
+    var name = ""
+    var ingredients = ""
+    var instructions = ""
+
+}
